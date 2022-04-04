a83c1964e971c2a4b662557ec8fd43a49259dcc9
Robert Burdett <46675270+rdburdett@users.noreply.github.com>
2022-04-04 14:39:41 -0500

    validation completed

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -11,8 +11,8 @@ const nextId = require("../utils/nextId");
 // EXISTS
 // * working
 function orderExists(req, res, next) {
-  const { orderId } = req.params
-  const foundOrder = orders.find((order) => order.id = Number(orderId))
+  const { orderId } = req.params;
+  const foundOrder = orders.find((order) => (order.id = Number(orderId)));
   if (foundOrder) {
     res.locals.order = foundOrder;
     return next();
@@ -24,16 +24,54 @@ function orderExists(req, res, next) {
 }
 
 // HASTEXT
-function hasText(req, res, next) {
-  res.sendStatus(404)
+function isValid(req, res, next) {
+  input = req.body.data;
+  let missingFieldsMessage = [];
+
+  if (!input.deliverTo || input.deliverTo.length === 0) {
+    missingFieldsMessage.push("Dish must include a deliverTo");
+  }
+  if (!input.mobileNumber || input.mobileNumber.length === 0) {
+    missingFieldsMessage.push("Dish must include a mobileNumber");
+  }
+  if (!input.dishes) {
+    missingFieldsMessage.push("Dish must include a dish");
+  }
+
+  if (input.dishes) {
+    if (input.dishes.length === 0 || !Array.isArray(input.dishes)) {
+      missingFieldsMessage.push("Order must include at least one dish");
+    } else {
+      input.dishes.forEach((dish, index) => {
+        if (dish.quantity <= 0 || !Number.isInteger(dish.quantity)) {
+          missingFieldsMessage.push(
+            `Dish ${index} must have a quantity that is an integer greater than 0`
+          );
+        }
+      });
+    }
+  }
+
+  if (missingFieldsMessage.length > 0) {
+    console.log(input.dishes);
+    next({
+      status: 400,
+      message: missingFieldsMessage.join(", "),
+    });
+  } else next();
 }
 
 //////////////////////////////
 
 // CREATE
 function create(req, res, next) {
-  
-  res.sendStatus(404)
+  const { data } = req.body;
+  const newOrder = {
+    id: nextId(),
+    ...data,
+  };
+  orders.push(newOrder);
+  res.status(201).json({ data: newOrder });
 }
 
 // READ
@@ -44,12 +82,12 @@ function read(req, res, next) {
 // UPDATE
 // (Do not allow order id to be overwritten)
 function update(req, res, next) {
-  res.sendStatus(404)
+  res.sendStatus(404);
 }
 
 // DELETE
 function destroy(req, res, next) {
-  res.sendStatus(404)
+  res.sendStatus(404);
 }
 
 // LIST
@@ -58,13 +96,12 @@ function list(req, res, next) {
   res.json({ data: orders });
 }
 
-
 //////////////////////////////
 
 module.exports = {
-  create: [hasText, create],
+  create: [isValid, create],
   read: [orderExists, read],
-  update: [orderExists, hasText, update],
+  update: [orderExists, isValid, update],
   delete: [orderExists, destroy],
   list: [list],
   orderExists,
